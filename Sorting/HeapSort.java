f5a066b9a7d22b6d52c2b69f31deb0e608660a79
Vijayendher Gatla <vijayendhergatla@Vijayendhers-MacBook-Air.local>
2024-02-14 19:20:24 +0530

    Solved Sorting Methods

---
+++ Sorting/HeapSort.java
@@ -0,0 +1,15 @@
+package Sorting;
+public class HeapSort {
+    public static void main(String[] args) {
+        int arr[]={10,7,6,12,3,8};
+        heapSort(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+
+    private static void heapSort(int[] arr) {
+        // TODO Auto-generated method stub
+    }
+    
+}
---
+++ Sorting/MergeSort.java
@@ -0,0 +1,16 @@
+package Sorting;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int arr[]={10,7,6,12,3,8};
+        mergeSort(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+
+    private static void mergeSort(int[] arr) {
+        // TODO Auto-generated method stub
+    }
+    
+}
